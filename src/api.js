e090ea9ce339a266f75c12e5d88b2d4bb93d7a84
ayana1709 <ayanabashachali@gmail.com>
2025-02-12 04:48:24 +0300

    setup for the deployment

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "https://speedmetergms.com/api/",
 });
 
 // Add CSRF token to every request header
