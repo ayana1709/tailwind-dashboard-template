7670fd926f6a130c35bc16c584abde7a8c392b32
ayana1709 <ayanabashachali@gmail.com>
2025-02-13 23:32:19 +0300

    adjusting the api for the uploading and removing  sanctum token

--- src/api.js
+++ src/api.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://speedmeter.rekikartgallery.com/api/",
+  baseURL: "https://speedmeter.rekikartgallery.com/api/",
   // baseURL: "https://gwg.rekikartgallery.com/api/",
-  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
   // baseURL: "https://speedmetergms.com/api/",
 });
 
 // Add CSRF token to every request header
-api.interceptors.request.use((config) => {
-  const csrfToken = document
-    .querySelector('meta[name="csrf-token"]')
-    ?.getAttribute("content");
+// api.interceptors.request.use((config) => {
+//   const csrfToken = document
+//     .querySelector('meta[name="csrf-token"]')
+//     ?.getAttribute("content");
 
-  if (csrfToken) {
-    config.headers["X-CSRF-TOKEN"] = csrfToken;
-  }
+//   if (csrfToken) {
+//     config.headers["X-CSRF-TOKEN"] = csrfToken;
+//   }
 
-  return config;
-});
+//   return config;
+// });
 
 export default api;
